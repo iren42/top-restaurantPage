b173e40f522d98aefcf0ed72d29a3bfa118a2b20
iren42 <isabelleren618@gmail.com>
2025-02-27 15:05:24 +0100

    feat: footer at the bottom

--- src/index.js
+++ src/index.js
@@ -5,4 +5,21 @@ import { aboutContent } from "./aboutContent.js";
 
 console.log("Hello");
 // loadContent(homeContent);
-loadContent(aboutContent);
+const homeBtn = document.querySelector("button#homeBtn");
+const menuBtn = document.querySelector("button#menuBtn");
+const aboutBtn = document.querySelector("button#aboutBtn");
+
+const content = document.querySelector("div#content");
+
+document.addEventListener("click", event =>
+{
+    event.preventDefault();
+    console.log(event.target.id);
+    if (event.target === homeBtn)
+    {
+        content.replaceChildren();
+        loadContent(homeContent);
+    }
+
+});
+// loadContent(aboutContent);
