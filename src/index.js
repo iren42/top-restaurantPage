a97ede8b1d33a2f516fa1a1ed0e534fe165d5c39
iren42 <isabelleren618@gmail.com>
2025-02-27 21:23:22 +0100

    fix: active nav button stays colored

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const menuBtn = document.querySelector("button#menuBtn");
 const aboutBtn = document.querySelector("button#aboutBtn");
 const brandBtn = document.querySelector("button#brandBtn");
 
+const navBtns = [
+    homeBtn,
+    menuBtn,
+    aboutBtn,
+];
 const content = document.querySelector("div#content");
 
 document.addEventListener("click", event =>
@@ -16,16 +21,30 @@ document.addEventListener("click", event =>
     // console.log(event.target);
     if (event.target === homeBtn || event.target === brandBtn)
     {
+        navBtns.forEach((nav) => {
+            nav.classList.remove("active");
+        });
+        homeBtn.classList.add("active");
         content.replaceChildren();
         loadContent(homeContent);
     }
     else if (event.target === menuBtn)
     {
+        navBtns.forEach((nav) =>
+        {
+            nav.classList.remove("active");
+        });
+        menuBtn.classList.add("active");
         content.replaceChildren();
         loadContent(menuContent);
     }
     else if (event.target === aboutBtn)
     {
+        navBtns.forEach((nav) =>
+        {
+            nav.classList.remove("active");
+        });
+        aboutBtn.classList.add("active");
         content.replaceChildren();
         loadContent(aboutContent);
     }
